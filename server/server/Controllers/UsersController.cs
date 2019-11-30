5bcc452ba07f5a6b8296aa6ec4def2346967dcc5
damjanvas <damjan.vas@gmail.com>
2019-11-30 20:05:50 +0100

    Section 13

--- server/server/Controllers/UsersController.cs
+++ server/server/Controllers/UsersController.cs
@@ -9,10 +9,12 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using server.Data;
 using server.Dtos;
+using server.Helpers;
 using server.Models;
 
 namespace server.Controllers
 {
+    [ServiceFilter(typeof(LogUserActivity))]
     [Authorize]
     [Route("api/users")]
     [ApiController]
--- server/server/Startup.cs
+++ server/server/Startup.cs
@@ -59,6 +59,7 @@ namespace server
             services.AddScoped<IDatingRepository, DatingRepository>();
             services.AddAutoMapper(typeof(Startup));
             services.Configure<CloudinarySettings>(Configuration.GetSection("CloudinarySettings"));
+            services.AddScoped<LogUserActivity>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
